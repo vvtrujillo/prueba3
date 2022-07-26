4c39e2009929f1b437c0a75cee504b3232bf7105
vvtrujillo <victor.valenzuela.t@gmail.com>
2022-07-26 19:40:12 -0400

    commit prueba 3

---
+++ src/Componentes/PersonCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+class PersonCard extends React.Component{
+    constructor(){
+        this.Nombre = this.Nombre;
+        this.Apellido = this.Apellido;
+        this.age = age;
+        this.hairColor=hairColor;
+    }
+    render(){
+        return(
+            <h1>{this.Nombre}</h1>
+        )
+    }
+
+}
+
+export default PersonCard;
